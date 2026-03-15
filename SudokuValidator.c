e8e3f3792f3b9821709fae8af4646ac5f137e642
ecarcamo <estebancarcamou@gmail.com>
2026-03-15 09:47:12 -0600

    feat: Add file opening and basic error handling for the input file.

--- SudokuValidator.c
+++ SudokuValidator.c
@@ -18,5 +18,15 @@ int main(int argc, char *argv[])
 
     printf("Archivo recibido: %s\n", argv[1]);
 
+    int file_descriptor = open(argv[1], O_RDONLY);
+
+    if (file_descriptor < 0)
+    {
+        perror("Error abriendo archivo");
+        return 1;
+    }
+
+    printf("Archivo abierto correctamente\n");
+
     return 0;
 }
